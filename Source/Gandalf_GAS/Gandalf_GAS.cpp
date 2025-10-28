b64eeaaa7a442aa4164c87c0080951ed64f008f8
Jakub Balcerzak <jakub.balcerzak.dv@gmail.com>
2025-10-28 16:28:24 +0100

    Initialize Project

---
+++ Source/Gandalf_GAS/Gandalf_GAS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gandalf_GAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gandalf_GAS, "Gandalf_GAS" );
