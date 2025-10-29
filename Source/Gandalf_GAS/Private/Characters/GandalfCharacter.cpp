1e5858359720c19442ff9cadbe9b610c144fb04d
Jakub Balcerzak <jakub.balcerzak.dv@gmail.com>
2025-10-29 19:07:25 +0100

    Added skeletons

--- Source/Gandalf_GAS/Private/Characters/GandalfCharacter.cpp
+++ Source/Gandalf_GAS/Private/Characters/GandalfCharacter.cpp
@@ -9,6 +9,9 @@ AGandalfCharacter::AGandalfCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "hand_r_Staff_Socket");  // TODO: move it to WeaponClass
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
